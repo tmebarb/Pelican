b6ef78c833390c5c56a3f85699299d5a4187eab1
tmebarb <tmebarb@gmail.com>
2016-02-27 22:03:45 -0600

    Subdirectory name change

--- ci-base/application/controllers/Dashboard.php
+++ ci-base/application/controllers/Dashboard.php
@@ -6,15 +6,21 @@
 			parent::__construct();
 			$this->load->model('Users_model');
 			$this->load->helper('url');
-		}
-
-		function index() {
+			
 			if(!$this->session->userdata('id')) {
 				$this->session->set_flashdata('error_msg', 'I can\'t remember u, Please login again! <br/><br/>');
 				redirect('login');
 			}
+		}
 
+		function index() {
 			$data = array('allusers' => $this->Users_model->getallusers(), 'view'=> 'admin-template/dashboard');
+
+			$this->load->view('admin', $data);
+		}
+
+		function db() {
+			$data = array('allusers' => $this->Users_model->getallusers(), 'view'=> 'admin-template/db');
 
 			$this->load->view('admin', $data);
 		}
