29db8e160b9954dcd6f49a693ba25db69edb17b0
roberswd <wesleyr2011@gmail.com>
2016-04-12 10:25:48 -0500

    Admin List function for Advisees
    Provides functionality to the admin->list all advisees link. Provides an
    advisees name, major, and classification using a select from where
    statement.

--- ci-base/application/views/admin-template/db.php
+++ ci-base/application/views/admin-template/db.php
@@ -11,19 +11,26 @@
                   <table class="striped">
                     <thead>
                       <tr>
-                        <th data-field="id">First Name</th>
-                        <th data-field="name">Last Name</th>
-                        <th data-field="price">Major</th>
+                        <th data-field="id">Name</th>
+                        <th data-field="name">Major</th>
+                        <th data-field="price">Classification</th>
                       </tr>
                     </thead>
-                    <tbody>
+                    <?php 
+                    $this->db->select('u.user_fullname, a.major, a.classification');
+                    $this->db->from('users u, advisee a');
+                    $this->db->where('a.user_id = u.user_id'); 
+                    $query = $this->db->get();
+                    ?>
 
-                      <?php foreach ($alladvisees as $currentRow): ?>
+                    <tbody>
+                      
+                      <?php foreach ($query->result() as $row): ?>
 
                       <tr>
-                        <td><?php echo $currentRow->FirstName ?></td>
-                        <td><?php echo $currentRow->LastName ?></td>
-                        <td><?php echo $currentRow->Major ?></td>
+                        <td><?php echo $row->user_fullname ?></td>
+                        <td><?php echo $row->major ?></td>
+                        <td><?php echo $row->classification ?></td>
                       </tr>
                         
                       <?php endforeach ?>
