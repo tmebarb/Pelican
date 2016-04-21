773a8c164423523e6b03ac80b850ee39b71a85a4
tannehke <tannehke@warhawks.ulm.edu>
2016-04-21 11:26:24 -0500

    Start/End Date Revision/Correction
    corrected start/end date inputs for timeslot submission page

--- ci-base/application/views/admin-template/sidebar.php
+++ ci-base/application/views/admin-template/sidebar.php
@@ -83,9 +83,11 @@
                       
                         <li class="bold"><a href="<?php echo base_url() ?>advisor/timeslots" class="waves-effect waves-cyan"><i class="mdi-action-alarm-on">
                            </i> Setup Timeslots (proper WIP)</a>
+                        <!--
                         </li>
-
+                        
                         <li class="bold"><a href="<?php echo base_url() ?>advisor/funTimeSlots" class="waves-effect waves-cyan"><i class="mdi-action-alarm-on">
+                          -->
                         </i> Setup Timeslots (functional)</a>
                         </li>
                       </ul>
