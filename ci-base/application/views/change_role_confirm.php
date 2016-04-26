77f5417a3c5011f816a19d584891f06900b442c5
tannehke <tannehke@warhawks.ulm.edu>
2016-04-25 22:15:33 -0500

    admin page/function changes
    added forgot password page, altered admin page, added admin change user
    roles function

---
+++ ci-base/application/views/change_role_confirm.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!--================================================================================
+	Item Name: Materialize - Material Design Admin Template
+	Version: 1.0
+	Author: GeeksLabs
+	Author URL: http://www.themeforest.net/user/geekslabs
+================================================================================ -->
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="msapplication-tap-highlight" content="no">
+  <meta name="description" content="Materialize is a Material Design Admin Template,It's modern, responsive and based on Material Design by Google. ">
+  <meta name="keywords" content="materialize, admin template, dashboard template, flat admin template, responsive admin template,">
+  <title>Cards | Pelican</title>
+
+  <!-- Favicons-->
+  <link rel="icon" href="images/favicon/favicon-32x32.png" sizes="32x32">
+  <!-- Favicons-->
+  <link rel="apple-touch-icon-precomposed" href="images/favicon/apple-touch-icon-152x152.png">
+  <!-- For iPhone -->
+  <meta name="msapplication-TileColor" content="#8c1919">
+  <meta name="msapplication-TileImage" content="images/favicon/mstile-144x144.png">
+  <!-- For Windows Phone -->
+
+
+  <!-- CORE CSS-->
+  
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection">
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection">
+
+
+  <!-- INCLUDED PLUGIN CSS ON THIS PAGE -->
+  <link href="css/prism.css" type="text/css" rel="stylesheet" media="screen,projection">
+  <link href="js/plugins/perfect-scrollbar/perfect-scrollbar.css" type="text/css" rel="stylesheet" media="screen,projection">
+  <link href="js/plugins/chartist-js/chartist.min.css" type="text/css" rel="stylesheet" media="screen,projection">
+</head>
+
+<body>
+  <!-- Start Page Loading -->
+  <div id="loader-wrapper">
+      <div id="loader"></div>        
+      <div class="loader-section section-left"></div>
+      <div class="loader-section section-right"></div>
+  </div>
+  <!-- End Page Loading -->
+
+  <!-- //////////////////////////////////////////////////////////////////////////// -->
+
+  <!-- START HEADER -->
+  <header id="header" class="page-topbar">
+        <!-- start header nav-->
+        <div class="navbar-fixed">
+            <nav class="cyan">
+                <div class="nav-wrapper">
+                    <h1 class="logo-wrapper"><a href="index.html" class="brand-logo darken-1"><img src="images/Pelican Logo.png" alt="materialize logo"></a> <span class="logo-text">Pelican</span></h1>
+                    <ul class="right hide-on-med-and-down">
+                        <li class="search-out">
+                            <input type="text" class="search-out-text">
+                        </li>
+                        <li>    
+                            <a href="javascript:void(0);" class="waves-effect waves-block waves-light show-search"><i class="mdi-action-search"></i></a>                              
+                        </li>
+                        <li><a href="javascript:void(0);" class="waves-effect waves-block waves-light toggle-fullscreen"><i class="mdi-action-settings-overscan"></i></a>
+                        </li>
+                        <li><a href="javascript:void(0);" class="waves-effect waves-block waves-light"><i class="mdi-social-notifications"></i></a>
+                        </li>
+                        <!-- Dropdown Trigger -->                        
+                        <li><a href="#" data-activates="chat-out" class="waves-effect waves-block waves-light chat-collapse"><i class="mdi-communication-chat"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <!-- end header nav-->
+  </header>
+  <!-- END HEADER -->
+
+  <!-- //////////////////////////////////////////////////////////////////////////// -->
+
+  <!-- START MAIN -->
+  <div id="main">
+    <!-- START WRAPPER -->
+    <div class="wrapper">
+
+    
+      <!-- START CONTENT -->
+      <section id="content">
+
+        <!--breadcrumbs start-->
+        <div id="breadcrumbs-wrapper" class=" grey lighten-3">
+          <div class="container">
+            <div class="row">
+              <div class="col s12 m12 l12">
+                <h5 class="breadcrumbs-title">Cards</h5>
+                <ol class="breadcrumb">
+                  <li><a href="dashboard">Dashboard</a>
+                  </li>
+                  <li><a href="#">Admin</a>
+                  </li>
+                  <li class="active">Success</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--breadcrumbs end-->
+
+
+        <!--start container-->
+        <div class="container">
+         
+          <div class="divider"></div>
+          <!--Basic Card-->
+          <div id="basic-card" class="section">
+          
+              <div class="col s12 m8 l9">
+                <div class="row">
+                  <div class="col s12 m8 l9">
+                    <div class="card  light-blue">
+                      <div class="card-content white-text">
+                        <span class="card-title">Success!</span>
+                        <p> The user with the ID <?php echo $user_id;?> was assigned the new role of <?php echo $user_type;?> .</p>
+                      </div>
+                      <div class="card-action">
+                        <a href="changeUserRole" class="lime-text text-accent-1">Back to Change User Role Page</a>
+                        <a href="dashboard" class="lime-text text-accent-1">Home</a>
+                      </div>
+                    </div>
+                  </div>
+                
+                  
+                 
+            </div>
+          </div>
+
+         
+               
+              
+          
+</div>
+        </div>
+        <!--end container-->
+
+      </section>
+      <!-- END CONTENT -->
+
+      
+    </div>
+    <!-- END WRAPPER -->
+
+  </div>
+  <!-- END MAIN -->
+
+
+
+
+    <!-- ================================================
+    Scripts
+    ================================================ -->
+    
+    <!-- jQuery Library -->
+    <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>    
+    <!--materialize js-->
+    <script type="text/javascript" src="js/materialize.js"></script>
+    <!--prism-->
+    <script type="text/javascript" src="js/prism.js"></script>
+    <!--scrollbar-->
+    <script type="text/javascript" src="js/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <!-- chartist -->
+    <script type="text/javascript" src="js/plugins/chartist-js/chartist.min.js"></script>   
+    
+    <!--plugins.js - Some Specific JS codes for Plugin Settings-->
+    <script type="text/javascript" src="js/plugins.js"></script>
+    
+</body>
+
---
+++ ci-base/application/views/forgot.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!--================================================================================
+  Item Name: Materialize - Material Design Admin Template
+  Version: 1.0
+  Author: GeeksLabs
+  Author URL: http://www.themeforest.net/user/geekslabs
+================================================================================ -->
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="msapplication-tap-highlight" content="no">
+  <meta name="description" content="Materialize is a Material Design Admin Template,It's modern, responsive and based on Material Design by Google. ">
+  <meta name="keywords" content="materialize, admin template, dashboard template, flat admin template, responsive admin template,">
+  <title>Register Page | Pelican</title>
+
+  <!-- Favicons-->
+  <link rel="icon" href="<?php echo base_url();?>asserts/images/favicon/favicon-32x32.png" sizes="32x32">
+  <!-- Favicons-->
+  <link rel="apple-touch-icon-precomposed" href="<?php echo base_url();?>asserts/images/favicon/apple-touch-icon-152x152.png">
+  <!-- For iPhone -->
+  <meta name="msapplication-TileColor" content="#8c1919">
+  <meta name="msapplication-TileImage" content="<?php echo base_url();?>asserts/images/favicon/mstile-144x144.png">
+  <!-- For Windows Phone -->
+
+
+  <!-- CORE CSS-->
+  
+  <link href="<?php echo base_url();?>asserts/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection">
+  <link href="<?php echo base_url();?>asserts/css/style.css" type="text/css" rel="stylesheet" media="screen,projection">
+  <link href="<?php echo base_url();?>asserts/css/page-center.css" type="text/css" rel="stylesheet" media="screen,projection">
+
+  <!-- INCLUDED PLUGIN CSS ON THIS PAGE -->
+  <link href="<?php echo base_url();?>asserts/css/prism.css" type="text/css" rel="stylesheet" media="screen,projection">
+  <link href="<?php echo base_url();?>asserts/js/plugins/perfect-scrollbar/perfect-scrollbar.css" type="text/css" rel="stylesheet" media="screen,projection">
+  
+</head>
+
+<body class="cyan">
+  <!-- Start Page Loading -->
+  <div id="loader-wrapper">
+      <div id="loader"></div>        
+      <div class="loader-section section-left"></div>
+      <div class="loader-section section-right"></div>
+  </div>
+  <!-- End Page Loading -->
+
+
+
+  <div id="login-page" class="row">
+    <?php echo validation_errors(); ?>
+    <?php echo form_open('login/forgot'); ?>
+
+    <?php echo $this->session->flashdata('error_msg') ?>
+    <?php echo $this->session->flashdata('success_msg') ?>
+    <div class="col s12 z-depth-4 card-panel">
+      <form class="login-form">
+        <div class="row">
+          <div class="input-field col s12 center">
+            <h4>Forgot your password?</h4>
+            <p class="center">Please enter your CWID and your new password.</p>
+          </div>
+        </div>
+        <div class="row margin">
+          <div class="input-field col s12">
+            <i class="mdi-social-person-outline prefix"></i>
+                <label for="CWID">
+                  Enter your Campus Wide ID: 
+                </label>
+                <?php
+                
+                $data = array(
+                  'name'        => 'CWID',
+                  'id'          => 'CWID',
+                  'style'   => 'margin-top: 10px; margin-left: 30px',
+                  'value'   => set_value('CWID'),
+                  );
+                echo form_input($data);
+                ?>
+          </div>
+        </div>
+        <div class="row margin">
+          <div class="input-field col s12">
+            <i class="mdi-action-lock-outline prefix"></i>
+              <label for="password">
+                New Password: 
+              </label>
+              <?php
+              $data = array(
+                'name'        => 'password',
+                'type'    => 'password',
+                'id'          => 'password',
+                'style'   => 'margin-top: 10px; margin-left: 30px'
+                );
+              echo form_input($data);?>
+              <br>
+          </div>
+        </div>
+        <div class="row margin">
+          <div class="input-field col s12">
+            <i class="mdi-action-lock-outline prefix"></i>
+
+              <label for="conpassword">
+                Confirm New Password: 
+              </label>
+              <?php
+              $data = array(
+                'name'        => 'con_password',
+                'type'    => 'password',
+                'id'          => 'con_password',
+                'style'   => 'margin-top: 10px; margin-left: 30px'
+                );
+              echo form_input($data);?>
+              <br>
+          </div>
+        </div>
+        
+        <!--
+        <div class="row margin">
+          <div class="input-field col s12">
+            <i class="mdi-social-person-outline prefix"></i>
+              <label for="user_email">
+                  University Email:                
+                  </label>
+                  <?php
+                  echo set_value('user_email');
+                  $data = array(
+                    'name'        => 'user_email',
+                    'id'          => 'user_email',
+                    'style'   => 'margin-top: 10px; margin-left: 30px',
+                    'value'   => set_value('user_email')
+                    );
+                  echo form_input($data);
+                  ?>
+              <br>
+          </div>
+        </div>
+        -->
+
+        <div class="row">
+          <div class="input-field col s12">
+           
+            <button type="submit" class="btn waves-effect waves-light col s12" name="submit" value="formSave">Submit</button>
+          </div>
+        
+
+          <div class="input-field col s12">
+            <p class="margin center medium-small sign-up">Return to <a href="<?php echo base_url()?>login">Login</a> page </p>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+
+
+
+  <!-- ================================================
+    Scripts
+    ================================================ -->
+
+  <!-- jQuery Library -->
+  <script type="text/javascript" src="<?php echo base_url();?>asserts/js/jquery-1.11.2.min.js"></script>
+  <!--materialize js-->
+  <script type="text/javascript" src="<?php echo base_url();?>asserts/js/materialize.js"></script>
+  <!--prism-->
+  <script type="text/javascript" src="<?php echo base_url();?>asserts/js/prism.js"></script>
+  <!--scrollbar-->
+  <script type="text/javascript" src="<?php echo base_url();?>asserts/js/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+
+  <!--plugins.js - Some Specific JS codes for Plugin Settings-->
+  <script type="text/javascript" src="<?php echo base_url();?>asserts/js/plugins.js"></script>
+
+</body>
+
+</html>
