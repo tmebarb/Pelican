7935a36902dda2d9fbea6e3461947c72745f75dc
Sohail Haider <sohailh343@gmail.com>
2016-05-02 05:19:01 -0500

    merged Advisees_model

--- ci-base/application/models/Advisees_model.php
+++ ci-base/application/models/Advisees_model.php
@@ -3,28 +3,31 @@
 class Advisees_model extends CI_Model
 {
 
-	function __construct()
-	{
-		parent::__construct();
-	}
-
-	function getAll() {
-		$query = $this->db->get('advisees');
-		return $query->result();
-	}
-	function getAllAdviseeDetails() {
-		$this->db->select('u.user_id, u.user_fullname, u.CWID, a.classification, a.major');
-		$this->db->from('users u');
-		$this->db->join('advisee a', 'u.user_id = a.user_id', 'inner');
-
-		$query = $this->db->get();
-		return $query->result();
-	}
-
-	//Function that inserts the user fullname, user name, email, cwid, password, phone number, classification, and major for an advisee into the user and advisee table
-	function saveAdvisee($user_fullname, $user_name, $email, $CWID, $password, $phone, $classification, $major)
+    function __construct()
     {
-    	//An array of information to be inserted into the users table
+        parent::__construct();
+    }
+
+    function getAll()
+    {
+        $query = $this->db->get('advisees');
+        return $query->result();
+    }
+
+    function getAllAdviseeDetails()
+    {
+        $this->db->select('u.user_id, u.user_fullname, u.CWID, a.classification, a.major');
+        $this->db->from('users u');
+        $this->db->join('advisee a', 'u.user_id = a.user_id', 'inner');
+
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    //Function that inserts the user fullname, user name, email, cwid, password, phone number, classification, and major for an advisee into the user and advisee table
+    function saveAdvisee($user_fullname, $user_name, $email, $CWID, $password, $phone, $classification, $major)
+    {
+        //An array of information to be inserted into the users table
         $user_data = array(
             'user_fullname' => $user_fullname,
             'user_name' => $user_name,
@@ -42,24 +45,26 @@ class Advisees_model extends CI_Model
 
         //An array of information for the same user that will be inserted into the advisee table
         $advisee_data = array(
-        	'classification' => $classification,
-        	'major' => $major,
-        	'user_id' => $advisee_userid
-        	);
+            'classification' => $classification,
+            'major' => $major,
+            'user_id' => $advisee_userid
+        );
         $this->db->insert('advisee', $advisee_data);
+    }
+    function getAdvisorDetailsByAdviseeUserID($user_id)
+    {
+        $this->db->select('u.advised_by as advisor_id, u2.user_fullname as advisor_name');
+        $this->db->from('users u, users u2');
+        $this->db->where('u.user_id', $user_id);
+        $this->db->where('u2.user_id = u.advised_by');
+        //$this->db->join('advisor a', 'u.user_id = a.user_id', 'inner');
 
-	function getAdvisorDetailsByAdviseeUserID($user_id) {
-		$this->db->select('u.advised_by as advisor_id, u2.user_fullname as advisor_name');
-		$this->db->from('users u, users u2');
-		$this->db->where('u.user_id', $user_id);
-		$this->db->where('u2.user_id = u.advised_by');
-		//$this->db->join('advisor a', 'u.user_id = a.user_id', 'inner');
+        $query = $this->db->get();
+        return $query->row();
+    }
 
-		$query = $this->db->get();
-		return $query->row();
-	}
-
-    function getAdvisingSessionDetailsbyAdvisorUserID($user_id) {
+    function getAdvisingSessionDetailsbyAdvisorUserID($user_id)
+    {
         $this->db->select('s.startDate, s.endDate, s.type');
         $this->db->from('active_advising_sessions s, users u, advisor a');
         $this->db->where('u.user_id', $user_id);
@@ -68,18 +73,19 @@ class Advisees_model extends CI_Model
         $query = $this->db->get();
         return $query->row();
     }
-    function getSlotsByDateNDay($day, $date, $advisor_id) {
+
+    function getSlotsByDateNDay($day, $date, $advisor_id)
+    {
         $this->db->select('s.slot_id, s.start_time, s.end_time');
         $this->db->from('timeslots s, active_advising_sessions actv, advisor a, users u');
         $this->db->where('u.user_id', $advisor_id);
         $this->db->where('u.user_id = a.user_id');
         $this->db->where('actv.endDate >=', $date);
         $this->db->where('s.day', $day);
-        $this->db->where('actv.startDate <=',   $date);
+        $this->db->where('actv.startDate <=', $date);
         $this->db->where('actv.advisor_id =a.advisor_id');
         $this->db->where('s.advisor_id =a.advisor_id');
         $query = $this->db->get();
         return $query->result();
     }
 }
-?>
