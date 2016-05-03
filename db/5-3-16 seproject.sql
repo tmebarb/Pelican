9417bbf4a020036740bc578161519d47b2b7ffc9
roberswd <wesleyr2011@gmail.com>
2016-05-03 06:45:24 -0500

    db for vdub

---
+++ db/5-3-16 seproject.sql
@@ -0,0 +1,187 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 03, 2016 at 01:44 PM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 5.6.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `seproject`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `active_advising_sessions`
+--
+
+CREATE TABLE `active_advising_sessions` (
+  `session_id` int(11) NOT NULL,
+  `startDate` date NOT NULL,
+  `endDate` date NOT NULL,
+  `type` int(11) NOT NULL,
+  `advisor_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `advisee`
+--
+
+CREATE TABLE `advisee` (
+  `advisee_id` int(11) NOT NULL,
+  `major` varchar(255) DEFAULT NULL,
+  `classification` varchar(32) DEFAULT NULL,
+  `hold` tinyint(1) NOT NULL DEFAULT '0',
+  `student_worker` tinyint(1) NOT NULL DEFAULT '0',
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `advisor`
+--
+
+CREATE TABLE `advisor` (
+  `advisor_id` int(11) NOT NULL,
+  `major` varchar(255) NOT NULL,
+  `office_loc` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `majors`
+--
+
+CREATE TABLE `majors` (
+  `major_code` varchar(255) NOT NULL,
+  `major_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `timeslots`
+--
+
+CREATE TABLE `timeslots` (
+  `slot_id` int(11) NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL,
+  `day` varchar(255) DEFAULT NULL,
+  `date` date NOT NULL,
+  `is_repeating` tinyint(1) NOT NULL DEFAULT '0',
+  `open` tinyint(1) NOT NULL DEFAULT '0',
+  `event` varchar(255) DEFAULT NULL,
+  `advisor_id` int(11) NOT NULL,
+  `advisee_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `CWID` int(8) NOT NULL,
+  `user_fullname` varchar(255) NOT NULL,
+  `user_name` varchar(32) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `user_phone` varchar(32) DEFAULT NULL,
+  `user_type` varchar(32) NOT NULL,
+  `advised_by` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `active_advising_sessions`
+--
+ALTER TABLE `active_advising_sessions`
+  ADD PRIMARY KEY (`session_id`);
+
+--
+-- Indexes for table `advisee`
+--
+ALTER TABLE `advisee`
+  ADD PRIMARY KEY (`advisee_id`);
+
+--
+-- Indexes for table `advisor`
+--
+ALTER TABLE `advisor`
+  ADD PRIMARY KEY (`advisor_id`);
+
+--
+-- Indexes for table `majors`
+--
+ALTER TABLE `majors`
+  ADD PRIMARY KEY (`major_code`);
+
+--
+-- Indexes for table `timeslots`
+--
+ALTER TABLE `timeslots`
+  ADD PRIMARY KEY (`slot_id`),
+  ADD UNIQUE KEY `Timeslot` (`start_time`,`end_time`,`day`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user-name` (`user_name`),
+  ADD UNIQUE KEY `CWID` (`CWID`,`user_name`,`user_email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `active_advising_sessions`
+--
+ALTER TABLE `active_advising_sessions`
+  MODIFY `session_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `advisee`
+--
+ALTER TABLE `advisee`
+  MODIFY `advisee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `advisor`
+--
+ALTER TABLE `advisor`
+  MODIFY `advisor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `timeslots`
+--
+ALTER TABLE `timeslots`
+  MODIFY `slot_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
